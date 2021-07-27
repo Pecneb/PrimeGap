cb174d4373575d249a48de5f6126e0c94ce62292
Pecneb <57943524+Pecneb@users.noreply.github.com>
2021-07-27 16:46:44 +0200

    adding comments to the program

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* 
+g is the gap size we want to find.
+m is the start number, n is the end number.
+The function returns the firs prime pair that has the g sized gap between them.
+We only looking for two primes after each other for example: 2-3, 3-5, 5-7...etc.
+If the function not finding primes between m and n, it returns {0, 0}.
+*/
 long long* gap(int g, long long m, long long n) {
+  // allocate memory for the two returning primes
   long long *ret = malloc(2*sizeof(long long));
   ret[0] = 0;
   ret[1] = 0;
   long long a = 0;
+  // This is the part when we use Sieve of Eratostheles
+  // enumerating all the primes until n
   int *primes = malloc(n*sizeof(int));
   primes[0] = 0;
   primes[1] = 0;
@@ -17,6 +27,7 @@ long long* gap(int g, long long m, long long n) {
       }
     }
   }
+  // Finding the first gap that equals g.
   for(long long i = m; i<n; i++) {
     if(primes[i] == 1) {
       printf("%lld, ", i);
